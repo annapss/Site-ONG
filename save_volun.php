924bb4eb3f27d8f263173d70cb608a73daa4694d
Maria Laura Soares <89169599+mlaurabs@users.noreply.github.com>
2022-02-15 11:20:29 -0300

    Delete save_volun.php

--- save_volun.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $nome = $_POST["nome"];
-    $email = $_POST["email"];
-    $telefone = $_POST["telefone"];
-    # ainda tem muitos outras info a serem pegas
-    
-    include "inserir_volun.php"
-    
-?>
