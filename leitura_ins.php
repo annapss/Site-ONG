c822b6ffa7616e1dca109fb38f079b32fb0f2046
zughy09 <mauro.pahoor@gmail.com>
2022-02-18 00:12:34 -0300

    Novo estilo tabela

--- leitura_ins.php
+++ leitura_ins.php
@@ -1,12 +1,6 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">    
 <!-- Bootstrap core CSS -->
 <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
-<style>
-    h1{
-        font-size: 40px;
-    }
-</style>
-
 <table border="1"  style="width:70%;" class="table table-dark table-striped" >
     <?php
 
@@ -15,7 +9,7 @@
     $arquivo = fopen("ins.csv", "r");
 
     if ($arquivo != FALSE){
-        echo "<h1>Inscrições enviadas</h1>";
+        echo "<h1 style='font-size: 40px;'>Inscrições enviadas</h1>";
         $dados = fgetcsv($arquivo, 1000, ";");
         echo "<tr>";
         echo "<th>Nome:</th>";
