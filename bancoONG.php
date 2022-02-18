d90e40134433be94fc05a9e3808a0577c0351582
Maria Laura Soares <89169599+mlaurabs@users.noreply.github.com>
2022-02-18 18:09:32 -0300

    Update bancoONG.php

--- bancoONG.php
+++ bancoONG.php
@@ -3,20 +3,28 @@
 include "conecta_banco.php";
 
 $sql = <<<FIM
-    CREATE DATABASE if NOT EXISTS grandes_altitudes;
+ CREATE DATABASE if NOT EXISTS grandes_altitudes;
+ CREATE TABLE grandes_altitudes.Login(
+            Email  varchar(40),
+            Senha  varchar(10),
+
+            PRIMARY KEY(Email)
+    );
     CREATE TABLE grandes_altitudes.Coordenador(
             Nome  varchar(50),
             Telefone  varchar(12),
             Email varchar(40),
         
-            PRIMARY KEY(Email)
+            PRIMARY KEY(Email),
+            FOREIGN KEY(Email) REFERENCES Login(Email)
     );
     CREATE TABLE grandes_altitudes.Voluntario(
             Nome varchar(50),
             Telefone  varchar(12),
             Email   varchar(40),
         
-            PRIMARY KEY(Email) 
+            PRIMARY KEY(Email), 
+            FOREIGN KEY(Email) REFERENCES Login(Email)
     );
     CREATE TABLE grandes_altitudes.Material_Feito(
             id_material int AUTO_INCREMENT,
