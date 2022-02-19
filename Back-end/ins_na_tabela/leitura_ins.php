fb069f9d3283a238bba5ae89f49d18143d06af9f
Maria Laura Soares <89169599+mlaurabs@users.noreply.github.com>
2022-02-19 18:17:54 -0300

    tabela inscricao já com bootstrap

--- Back-end/ins_na_tabela/leitura_ins.php
+++ Back-end/ins_na_tabela/leitura_ins.php
@@ -1,3 +1,4 @@
+//arquivo da tabela com os dados salvos na inscricao
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">    
 <!-- Bootstrap core CSS -->
 <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
