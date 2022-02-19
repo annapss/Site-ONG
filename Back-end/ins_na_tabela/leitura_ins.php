8333cf8578371b15f3f7283f3a5981ce0e3066db
Maria Laura Soares <89169599+mlaurabs@users.noreply.github.com>
2022-02-19 18:25:09 -0300

    arquivo para leitura do ins.csv

--- Back-end/ins_na_tabela/leitura_ins.php
+++ Back-end/ins_na_tabela/leitura_ins.php
@@ -1,10 +1,10 @@
-//arquivo da tabela com os dados salvos na inscricao
+
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">    
 <!-- Bootstrap core CSS -->
 <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
 <table border="1"  style="width:70%;" class="table table-dark table-striped" >
     <?php
-
+    //arquivo da tabela com os dados salvos na inscricao
     $linha = 1;
     //Nome; Telefone; Participacao; Dia_hora; Grau; Dia_hora_opcao; Materias; Outros
     $arquivo = fopen("ins.csv", "r");
