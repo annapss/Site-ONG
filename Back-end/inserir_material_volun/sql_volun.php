c1bcec67dcd9591b11274b66c76cd9098fa86044
victor-kaua <victorkauamartinsnun@gmail.com>
2022-02-19 22:02:42 -0300

    _

--- Back-end/inserir_material_volun/sql_volun.php
+++ Back-end/inserir_material_volun/sql_volun.php
@@ -3,10 +3,7 @@
 // arquivo com o sql necessario para consulta ou inserção na tabela voluntario do banco
 
 $material =<<<FIM
-
     INSERT INTO grandes_altitudes.material_feito (Materia, Conteudo, Formato, Feita_por) VALUES ("$materia", "$conteudo", "$formato", "$autor");
-
-
 FIM;
 
 
