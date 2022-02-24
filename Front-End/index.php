a241b7e91a72507f0600f862067c0d33d813018d
victor-kaua <victorkauamartinsnun@gmail.com>
2022-02-24 20:43:30 -0300

    _

--- Front-End/index.php
+++ Front-End/index.php
@@ -48,7 +48,7 @@
         <ul class="navbar-nav me-auto mb-2 mb-md-0">
           <li class="nav-item">
             <!--class="nav-link active"-->
-            <a class="nav-link" aria-current="page" href="materias.php">Matérias</a>
+            <a class="nav-link" aria-current="page" href="materias_front/materias.php">Matérias</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="https://www.youtube.com/channel/UCaTf49HZzLhWikOMC9ofm4A" target="_blank">Youtube</a>
@@ -146,7 +146,7 @@
       <div class="col-md-7">
         <h2 class="featurette-heading"> Matérias</h2>
         <p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
-        <a class="btn btn-primary" href="Front-End/materias_front/materias.php" role="button">Matérias</a>
+        <a class="btn btn-primary" href="materias_front/materias.php" role="button">Matérias</a>
       </div>
       <div class="col-md-5">
         <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
--- Front-End/materias_front/materias.php
+++ Front-End/materias_front/materias.php
@@ -40,7 +40,7 @@
 <header>
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="index.php">Grandes A(l)titudes</a>
+      <a class="navbar-brand" href="Front-End/index.php">Grandes A(l)titudes</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -48,7 +48,7 @@
         <ul class="navbar-nav me-auto mb-2 mb-md-0">
           <li class="nav-item">
             <!--class="nav-link active"-->
-            <a class="nav-link" aria-current="page" href="Front-End/materias_front/materias.php">Matérias</a>
+            <a class="nav-link" aria-current="page" href="materias.php">Matérias</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="https://www.youtube.com/channel/UCaTf49HZzLhWikOMC9ofm4A" target="_blank">Youtube</a>
