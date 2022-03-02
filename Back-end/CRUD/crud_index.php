fef3aef2ea76868585545801419d2adc3ff420de
zughy09 <mauro.pahoor@gmail.com>
2022-03-02 10:21:54 -0300

    Organização pastas Crud

--- Back-end/CRUD/crud_index.php
+++ Back-end/CRUD/crud_index.php
@@ -2,29 +2,9 @@
     <head>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">    
         <?php include 'C:\xampp\htdocs\Site_ONG\Front-End\assets\xmenu.php'; /* Lindinho capaz de substituir o header s2 */ ?> 
-        <style> 
-            label{
-                font-size: 16px;
-            }
-            .body{
-                text-align: center;
-                margin: 0% 30% 0% 30%;
-                background-color: #424757;
-                padding: 20px;
-                color: #efefef;
-                font-family: "Open Sans", Arial, sans-serif;
-            }
-            .contornar{
-                color:#FFCCAC; 
-                text-shadow:#000 2px -2px, #000 -2px 2px, #000 2px 2px, #000 -2px -2px;
-            }
-            h1,h2,h3,h4,h5,h6{
-                color: #efefef;
-                font-weight: bold;
-            }
-        </style>
+        <link rel="stylesheet" href="assets\crud.css">
     </head>
-    <body style="background-color:blanchedalmond;">
+    <body>
         <div class="body contornar" style="background-color: #73cee3;">
             <br><br><br>
             <h1>Crud</h1>
