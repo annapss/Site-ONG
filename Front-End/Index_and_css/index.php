cb30646e127085c258f5e95f715fa0fe318ec71d
victor-kaua <victorkauamartinsnun@gmail.com>
2022-03-06 00:34:42 -0300

    .

--- Front-End/Index_and_css/index.php
+++ Front-End/Index_and_css/index.php
@@ -42,11 +42,13 @@
 <main>
 
   <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-indicators">
+    <div class="carousel-indicators"> <!-- indicadores de indices(os tracinhos cinza)-->
       <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
       <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
     </div>
-    <div class="carousel-inner">
+    <div class="carousel-inner"> <!-- barra de navegação -->
+    <!-- Redirecionamento para as informações sobre a ONG -->
       <div class="carousel-item active">
         <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
         <div class="container">
@@ -57,6 +59,9 @@
           </div>
         </div>
       </div>
+      <!-- Fim div -->
+
+      <!-- Redirecionamento para a página de inscrição de novos voluntários -->
       <div class="carousel-item">
         <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
         <div class="container">
@@ -67,7 +72,23 @@
           </div>
         </div>
       </div>
+      <!-- Final div -->
+
+      <!-- Perguntas Frequentes -->
+      <div class="carousel-item"> <!-- Ideia pra fazer um lugar de Dúvidas frequentes e para esclarecer dúvidas de futuros voluntários/curiosos/estudantes-->
+        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
+        <div class="container">
+          <div class="carousel-caption">
+            <h1>Possui alguma Dúvida?</h1>
+            <p>Consulte as Dúvidas mais Frequentes!</p>
+            <p><a class="btn btn-lg btn-primary" href="#">Dúvidas Frequentes</a></p> 
+          </div>
+        </div>
+      </div>
+      <!-- Final div -->
+
     </div>
+    <!-- botões de Next e Prox "<" ">" --> 
     <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Previous</span>
@@ -88,25 +109,28 @@
     <!-- Three columns of text below the carousel -->
     <div class="row">
       <div class="col-lg-4">
-        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-
-        <h2>Depoimento 1</h2>
-        <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
-        <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
+        <!-- <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg> -->
+        <img src="../../Imagens_and_Icons/clei.png" class="bd-placeholder-img rounded-circle" width="140" height="140">
+        <h2>Cleiton Rasta</h2>
+        <p>INCRÍVEL IGUAL AS MINHAS PEDRADAS!</p>
+        <p>Graças ao material disponibilizado, consegui entrar no melhor da UFRJ! Especialização no jogo do bicho. Atualmente, estou realizando meu sonho, doutorado no jogo do bicho,grato eternamente a ONG.</p>
+        <p><i>#vitoriachegoudeusabencoou</i></p> 
+        <p><a class="btn btn-secondary" href="https://youtu.be/hzd-ziP0sBQ"> s2 &raquo;</a></p>
       </div><!-- /.col-lg-4 -->
       
       <div class="col-lg-4">
         <!--<svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>-->
-        <img src="../../Imagens_and_Icons/pessoaA.png" class="bd-placeholder-img rounded-circle" width="140" height="140">
-        <h2>Depoimento 2</h2>
-        <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
-        <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
+        <img src="../../Imagens_and_Icons/depoiment_real.png" class="bd-placeholder-img rounded-circle" width="140" height="140">
+        <h2>Helena</h2>
+        <p>O mérito do meu ingresso na UFF,dedico totalmente a essa ONG maravilhosa. Hoje sou formada em Engenharia da computação, mas conheci a ONG quando tinha apenas 18 anos. Os resumos são incíveis, altamente detalhados e completos! Muito obrigado a todos que colaboram até hoje para esse projeto surreal!</p>
+        <p>Vocês são Incíveis. :)</p>
+        <p><a class="btn btn-secondary" href="#">Veja mais depoimentos&raquo;</a></p>
       </div><!-- /.col-lg-4 -->
       
       <div class="col-lg-4">
         <!--<svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>-->
         <img src="../../Imagens_and_Icons/mulher.jpg" class="bd-placeholder-img rounded-circle" width="140" height="140">
-        <h2>Depoimento 3</h2>
+        <h2>Giulia</h2>
         <p>And lastly this, the third column of representative placeholder content.</p>
         <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
       </div><!-- /.col-lg-4 -->
