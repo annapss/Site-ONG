fba1e7521c4a55259e734a370a359457a0291b90
victor-kaua <victorkauamartinsnun@gmail.com>
2022-03-07 01:02:33 -0300

    Consult+Insert com o seu titulo de pag

--- Back-end/consulta_material_volu/consult_material_volu.php
+++ Back-end/consulta_material_volu/consult_material_volu.php
@@ -5,6 +5,7 @@
         <link href="../../Front-End/assets/dist/css/bootstrap.min.css" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">    
         <link rel="stylesheet" href="../assets/css/site.css">
+        <title>Consultar Material</title>
     </head>
     <body style="background-color:blanchedalmond;">
         <div class="body contornar" style="background-color: #73cee3;">
--- Back-end/inserir_material_volun/insert_material_index.php
+++ Back-end/inserir_material_volun/insert_material_index.php
@@ -3,6 +3,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">    
         <!--php include 'C:\xampp\htdocs\Site-ONG\Front-End\assets\xmenu.php';-->
         <link rel="stylesheet" href="../assets/css/site.css">
+        <title>Inserir Material</title>
     </head>
     <body>
         <div class="body contornar" style="background-color: #73cee3;">
