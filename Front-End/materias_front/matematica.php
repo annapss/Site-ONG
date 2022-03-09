4543fece33adc866f2009f8d3de1b1a242b55d47
Rafaela Bezerra de Figueiredo <rafaelabezerra2004@gmail.com>
2022-03-09 18:05:58 -0300

    Fundo da pag matematica

--- Front-End/materias_front/matematica.php
+++ Front-End/materias_front/matematica.php
@@ -45,7 +45,7 @@
     <div class="carousel-inner">
       
       <div class="carousel-item active">
-        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
+        <img class="bd-placeholder-img" width="100%" height="100%" src="../../Imagens_and_Icons/FundoPagMatematica.jpg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></img>
 
         <div class="container">
           <div class="carousel-caption">
